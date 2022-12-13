51e1f42fa7fbdea6fd4c17b625b09a2294e7ab8e
Kyle Marsh <kyle.marsh@microsoft.com>
2022-12-13 07:41:54 -0800

    Create config.cs

---
+++ config.cs
@@ -0,0 +1,4 @@
+"AadClientId": "b07420a2-3150-4f2f-aedc-9833aa195726",
+"AadSecret": "6kU8Q~L~~Wg~qzfKNG53xNAi0nTMLfJuZTWxAcGD",
+"AadTenantDomain": "idfordevs.dev",
+"AadTenantId": "833ced3d-cb2e-41de-92f1-29e2af035ddc",
